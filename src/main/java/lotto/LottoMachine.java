b76532dcb85614bb50cf1d2d201fb8f6765d64e8
shlihs95 <fhzptvh@gmail.com>
2025-11-03 22:13:46 +0900

    feat(application): LottoMachine 구현(Randoms.pickUniqueNumbersInRange 사용)

---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoMachine {
+
+    public List<Lotto> issue(PurchaseAmount amount) {
+        int count = amount.countTickets();
+        List<Lotto> tickets = new ArrayList<>(count);
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            tickets.add(new Lotto(numbers));
+        }
+
+        return tickets;
+    }
+}
